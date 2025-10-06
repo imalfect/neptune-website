f05b186df907cac6ba00706ac8c8cfb57c6fe596
iMalFect <me@imal.dev>
2025-10-06 16:26:27 +0200

    add metadata to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,43 @@ import Hero from '@/sections/Hero';
 import Introduction from '@/sections/Introduction';
 import Resources from '@/sections/Resources';
 import Tokenomics from '@/sections/Tokenomics';
-
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+	title: 'Neptune Cash',
+	description:
+		'Anonymous, succinct. Neptune Cash is a layer-1 peer-to-peer anonymou cash protocol featuring privacy, scalability and post-quantum security.',
+	openGraph: {
+		title: 'Neptune Cash',
+		description:
+			'Anonymous, succinct. Neptune Cash is a layer-1 peer-to-peer anonymous cash protocol featuring privacy, scalability and post-quantum security.',
+		siteName: 'Neptune Cash',
+		images: [
+			{
+				url: '/og-image.png', // TODO - replace with absolute URL in production
+				width: 1200,
+				height: 630,
+				alt: 'Neptune Cash'
+			}
+		],
+		locale: 'en_US',
+		type: 'website'
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Neptune Cash',
+		description:
+			'Anonymous, succinct. Neptune Cash is a layer-1 peer-to-peer anonymous cash protocol featuring privacy, scalability and post-quantum security.',
+		images: ['/og-image.png'] // TODO - this will not work now!!, replace with absolute URL in production
+	},
+	alternates: {
+		canonical: 'https://neptune.cash/'
+	},
+	robots: {
+		index: true,
+		follow: true
+	},
+	category: 'cryptocurrency'
+};
 export default function Home() {
 	console.log('Home page rendered');
 	return (
