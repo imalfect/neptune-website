2252405b16bb4bfe08a645c43e2da149452081da
iMalFect <me@imal.dev>
2025-10-09 21:06:50 +0200

    remove redundant animated logo loading
    never used anyway

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import AnimatedLogo from '@/components/resources/AnimatedLogo';
-
-export default function Loading() {
-	return (
-		<div className="fixed inset-0 z-50 flex items-center justify-center bg-[#040a1c]">
-			<AnimatedLogo />
-		</div>
-	);
-}
