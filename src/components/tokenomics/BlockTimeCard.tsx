f87cfc5fa05a7b5928f8a835381f5399bf1c0688
iMalFect <me@imal.dev>
2025-10-12 01:13:17 +0200

    update block time text

--- src/components/tokenomics/BlockTimeCard.tsx
+++ src/components/tokenomics/BlockTimeCard.tsx
@@ -2,13 +2,16 @@ import AeroCard from '../cards/AeroCard';
 
 export default function BlockTimeCard() {
 	return (
-		<AeroCard title={'Block time'} description={'9.8 minute thing '} width={'800px'}>
+		<AeroCard
+			title={'Block time'}
+			description={"Target block time and why it's high"}
+			width={'800px'}
+		>
 			<div className="flex flex-col gap-8">
 				<div className="text-xl">
 					Neptune has a target block time of 9.8 minutes, which is the average time it takes for a new
-					block to be added to the blockchain. This block time is a balance between transaction speed and
-					network security, ensuring that transactions are confirmed in a timely manner while maintaining
-					the integrity of the network.
+					block to be added to the blockchain. This reason for such a high block time is that block
+					proofs take a long time to build. It&apos;s over a minute per a coinbase transaction.
 				</div>
 			</div>
 		</AeroCard>
