dda18b3b11fd4f6adf3920845c1a77bec42bbdb4
iMalFect <me@imal.dev>
2025-10-18 00:30:20 +0200

    Revert "disable cache interception"
    This reverts commit 3b49bb94a6f09837bee3ac1f79d37d424aab0b6a.

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,5 +3,6 @@ import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
 import staticAssetsIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache';
 
 export default defineCloudflareConfig({
-	incrementalCache: staticAssetsIncrementalCache
+	incrementalCache: staticAssetsIncrementalCache,
+	enableCacheInterception: true
 });
