1e5aee246b39cc93f70d4d80a73e7e4c4641cabe
iMalFect <me@imal.dev>
2025-10-18 16:54:31 +0200

    rename token to coin

--- src/sections/Fundamentals.tsx
+++ src/sections/Fundamentals.tsx
@@ -15,7 +15,7 @@ export default function Fundamentals() {
 			<div className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
 				<FundamentalElement
 					title="Supply Limit"
-					description="The asymptotic (hard) limit on the token supply is 42 million coins. The concrete number is irrelevant, instead the fact that this limit exists is the relevant feature. It guarantees that once you own a share of the supply, that share will never shrink."
+					description="The asymptotic (hard) limit on the coin supply is 42 million coins. The concrete number is irrelevant, instead the fact that this limit exists is the relevant feature. It guarantees that once you own a share of the supply, that share will never shrink."
 					icon={<LucideCoins className="h-8 w-8 text-white" />}
 				/>
 				<FundamentalElement
